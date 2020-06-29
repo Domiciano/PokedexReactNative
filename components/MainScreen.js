647ee328a2ead37a753bc02750e23a019a622ee2
DOMICIANO RINCON <domiciano.rincon@correounivalle.edu.co>
2020-06-29 15:52:05 -0500

    Navigation

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,95 +1,57 @@
-/*This is an Example to make a View Like Android Fragment in React Native*/
-import React, {Component, Suspense, lazy} from 'react';
-import { Image, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
-import {styles} from '../styles/styles.js';
+import * as React from 'react';
+import { Image, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {styles} from '../styles/styles.js'
+import First from '../fragments/FirstScreen'
+import Second from '../fragments/SecondScreen'
+import Third from '../fragments/ThirdScreen'
 
-/*
-import FirstScreen from '../fragments/FirstScreen';
-import SecondScreen from '../fragments/SecondScreen';
-import ThirdScreen from '../fragments/ThirdScreen';
-*/
+const Tab = createBottomTabNavigator();
 
-
-
-
-
-
-const FirstScreen = lazy(()=>import('../fragments/FirstScreen'));
-const SecondScreen = lazy(()=>import('../fragments/SecondScreen'));
-const ThirdScreen = lazy(()=>import('../fragments/ThirdScreen'));
+export default function App() {
+  return (
+    
 
 
-export default class MainScreen extends Component {
-  constructor(props) {
-    super(props);
-    //state to manage the screen visible at a time
-    this.state = { val: 1 };
-  }
 
 
-  renderElement() {
-    if (this.state.val === 1) {
-      return (
-          <Suspense fallback={<Text>Loading...</Text>}>
-            <FirstScreen/>
-          </Suspense>
-        );
-    } else if (this.state.val === 2) {
-      return (
-        <Suspense fallback={<Text>Loading...</Text>}>
-          <SecondScreen/>
-        </Suspense>
-        );
-    } else {
-      return (
-        <Suspense fallback={<Text>Loading...</Text>}>
-          <ThirdScreen/>
-        </Suspense>
-        );
-    }
-  }
 
-  
+      <Tab.Navigator
+          screenOptions={
+            ({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === 'First') {
+                iconName = require('../images/qr.png');
+              } else if (route.name === 'Second') {
+                iconName = require('../images/user.png');
+              } else if (route.name === 'Third') {
+                iconName = require('../images/verify.png');
+              }
 
-  render() {
-    return (
-      <View style={styles.container}>
-
-
-        <View style={styles.fragmentContainer}>
-          {this.renderElement()}
-        </View>
-
-        <View style={styles.bottomBar}>
-          
-          <TouchableOpacity
-            onPress={() => this.setState({ val: 1 })}>
-            <Image 
-              style={this.state.val==1?styles.barButtonSelected:styles.barButton} 
-              source={require('../images/qr.png')}
-            />
-          </TouchableOpacity>
-          
-          <TouchableOpacity
-            onPress={() => this.setState({ val: 2 })}>
-            <Image 
-              style={this.state.val==2?styles.barButtonSelected:styles.barButton} 
-              source={require('../images/user.png')}
-            />
-          </TouchableOpacity>
-          
-          <TouchableOpacity
-            onPress={() => this.setState({ val: 3 })}>
-            <Image 
-              style={this.state.val==3?styles.barButtonSelected:styles.barButton} 
-              source={require('../images/verify.png')}
-            />
-          </TouchableOpacity>
-
-        </View>
-
-
-      </View>
-    );
-  }
+            // You can return any component that you like here!
+            return (
+              <Image
+                style={focused?styles.tinyImage:styles.tinyImageSelected}
+                source={iconName}
+              />
+            );
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+          style: {
+            backgroundColor: 'white',
+          },
+        }}
+        
+      >
+        <Tab.Screen name="First" component={First} />
+        <Tab.Screen name="Second" component={Second} />
+        <Tab.Screen name="Third" component={Third} />
+      </Tab.Navigator>
+    
+  );
 }
