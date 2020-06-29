2e967be05516c50efad1b01ef598b8d444e7a03a
DOMICIANO RINCON <domiciano.rincon@correounivalle.edu.co>
2020-06-29 16:35:39 -0500

    Navigation

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,11 +1,26 @@
-import React, {Component} from 'react';
+import React, {Component,Suspense, lazy} from 'react';
 import { Image, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {styles} from '../styles/styles.js'
-import First from '../fragments/FirstScreen'
-import Second from '../fragments/SecondScreen'
-import Third from '../fragments/ThirdScreen'
+
+
+
+
+//Esta es la forma convencional de importar componentes, pero si se quiere cargar
+//estilo lazy loading, además de poner un pequeño mensaje de carga mientras el componente se muestras
+//es mejor, porque aporta a la UX.
+
+//import Third from '../fragments/ThirdScreen'
+//import First from '../fragments/FirstScreen'
+//import Second from '../fragments/SecondScreen'
+
+//Las importaciones lazy se hacen asi:
+const First = lazy(()=>import('../fragments/FirstScreen'));
+const Second = lazy(()=>import('../fragments/SecondScreen'));
+const Third = lazy(()=>import('../fragments/ThirdScreen'));
+
+
 
 const Tab = createBottomTabNavigator();
 
@@ -34,8 +49,6 @@ export default class MainScreen extends Component{
               } else if (route.name === 'Third') {
                 iconName = require('../images/verify.png');
               }
-
-            // You can return any component that you like here!
             return (
               <Image
                 style={focused?styles.tinyImage:styles.tinyImageSelected}
@@ -51,17 +64,32 @@ export default class MainScreen extends Component{
             backgroundColor: 'white',
           },
         }}
-        
       >
+
         <Tab.Screen name="First">
-          {() => <First userName={this.props.route.params.username}/>}
+          {() => <Suspense fallback={<Text>Loading...</Text>}><First userName={this.props.route.params.username}/></Suspense>}
         </Tab.Screen>
         <Tab.Screen name="Second">
-          {() => <Second/>}
+          {() => <Suspense fallback={<Text>Loading...</Text>}><Second/></Suspense>}
         </Tab.Screen>
         <Tab.Screen name="Third">
-          {() => <Third/>}
+          {() => <Suspense fallback={<Text>Loading...</Text>}><Third/></Suspense>}
         </Tab.Screen>
+
+        {
+
+          /* 
+          Cuando se hace un lazy loading, se debe encerrar el componente en los tags <Suspense>
+          Alli se usa la propiedad fallback para poner lo que se quiere mostrar mientras carga
+          el componente
+          */
+          
+          /* 
+          Los Screens tambien puede ponerse usando
+          <Tab.Screen name="Second" component={Second}/>
+          */
+        }
+
       </Tab.Navigator>  
     );
   }
